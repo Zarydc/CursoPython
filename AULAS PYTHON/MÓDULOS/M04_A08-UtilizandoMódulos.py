6208387139635ea9501048c8f0a83b175c887e37
Zarydc <dany.engcarrera@gmail.com>
2023-04-30 22:06:43 -0300

    salva ae

---
+++ AULAS PYTHON/MÓDULOS/M04_A08-UtilizandoMódulos.py
@@ -0,0 +1,10 @@
+#------MÓDULO 4: AULA 08 - UTILIZANDO MÓDULOS------
+import math #Importa TODOS os itens da bliblioteca 'math' 
+num = int(input("\nDigite um número inteiro: "))
+raiz = math.sqrt(num) #Chama a função para calcular raiz quadrada (sqrt).
+
+print('\nA raiz quadrada de {} é igual a {}.\n'.format(num, raiz)) #mostrará a raiz quadrada com todas as casas decimais.
+
+print('\nResultados da raiz quadrada de {}:\n\n (a) Arredondado para cima: {:.2f}'.format(num, math.ceil(raiz))) #Mostrará o resultado da raiz arredondado para cima.
+print('\n (b) Arredondado para baixo: {:.2f}'.format(math.floor(raiz))) #Mostra a raiz arredondada para baixo.
+print('\n (c) Sem arredondamento: {:.2f}'.format(raiz))
