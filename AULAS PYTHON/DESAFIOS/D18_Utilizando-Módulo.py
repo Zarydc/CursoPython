3ad4422f610c03eb38d6ff855af16d1bc5438e11
Zarydc <dany.engcarrera@gmail.com>
2023-05-03 14:09:12 -0300

    HJ 03-05-2023

--- AULAS PYTHON/DESAFIOS/D18_Utilizando-Módulo.py
+++ AULAS PYTHON/DESAFIOS/D18_Utilizando-Módulo.py
@@ -1,1 +1,11 @@
-#DESAFIO 18: Faça um programa que leia um ângulo qualquer r mostre na tela o valo
+#DESAFIO 18: Faça um programa que leia um ângulo qualquer e mostre na tela o valor 
+# do seno, cosseno e tangente desse ângulo.
+
+from math import cos, sin, tan
+ang = float(input('\n\nInsira um ângulo: '))
+
+Cos = cos(ang)
+Sin = sin(ang)
+Tan = tan(ang)
+
+print('\n\nO ângulo é {}°\ncos({}°) = {}\nsin({}°) = {}\ntan({}°) = {}\n\n\n'.format(ang, ang, Cos, ang, Sin, ang, Tan))
---
+++ AULAS PYTHON/DESAFIOS/D19_Utilizando-Módulos.py
@@ -0,0 +1,4 @@
+#Um professor quer sortear um dos seus QUATRO alunos para apagar o quadro. 
+#Faça um programa que ajude ele, lendo o nome deles e escrevendo o nome do escolhido.
+
+import random
